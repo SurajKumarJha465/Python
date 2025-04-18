2ac4a654fdd4ccfd13d16360558ffe01f9158afc
Suraj kumar <suraj.231537@ncit.edu.np>
2025-04-18 22:33:09 +0545

    Use of sis and argv function

---
+++ 4-Day (Libraries)/Statistics.py
@@ -0,0 +1,15 @@
+# python provide statistics function to calculate mean, median, mode, and standard deviation of a dataset
+
+import statistics
+
+print(statistics.mean([2,3,45,6,7]))
+
+#sys = system
+#argv =  argument vector
+
+# command line argument
+
+import sys
+
+print("Hello, my name is",sys.argv[1])
+
